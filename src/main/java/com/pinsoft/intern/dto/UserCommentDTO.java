730861d1f6a1bf3fa5fe1db6e17f67e453cf9d45
Bekir <bekiryildirim.2508@gmail.com>
2024-11-13 21:06:43 +0300

    added new dto

---
+++ src/main/java/com/pinsoft/intern/dto/UserCommentDTO.java
@@ -0,0 +1,13 @@
+package com.pinsoft.intern.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCommentDTO {
+    private String username;
+    private String image;
+}
