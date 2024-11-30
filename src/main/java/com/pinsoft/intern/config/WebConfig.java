734fbbf2789a93e334c753ce581bd027491482df
Bekir <bekiryildirim.2508@gmail.com>
2024-11-30 20:15:32 +0300

    fixed

--- src/main/java/com/pinsoft/intern/config/WebConfig.java
+++ src/main/java/com/pinsoft/intern/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Tüm endpoint'ler için CORS ayarları
                 .allowedOrigins("http://localhost:5173", "https://blogger-pinsoft.vercel.app/") // İzin verilen kaynaklar
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // İzin verilen HTTP yöntemleri
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // İzin verilen HTTP yöntemleri
                 .allowedHeaders("*") // Tüm başlıklar
                 .allowCredentials(true); // Kimlik bilgilerine izin verme
     }
